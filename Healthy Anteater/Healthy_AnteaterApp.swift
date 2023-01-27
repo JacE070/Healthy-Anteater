e19d40d5114f1b6115866210975e9f15e6894570
JacE070 <luo0102ping@gmail.com>
2023-01-26 19:43:24 -0800

    Initial Commit

---
+++ Healthy Anteater/Healthy_AnteaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Healthy_AnteaterApp.swift
+//  Healthy Anteater
+//
+//  Created by JacEL on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Healthy_AnteaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

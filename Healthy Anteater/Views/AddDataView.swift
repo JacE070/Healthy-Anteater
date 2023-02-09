932f095fc5b03d9d90a414d2c5b8c873b3e2d627
Junping Luo <53324036+JacE070@users.noreply.github.com>
2023-02-08 20:33:08 -0800

    Finished RecView UI

---
+++ Healthy Anteater/Views/AddDataView.swift
@@ -0,0 +1,20 @@
+//
+//  AddDataView.swift
+//  Healthy Anteater
+//
+//  Created by JacEL on 2/8/23.
+//
+
+import SwiftUI
+
+struct AddDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddDataView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddDataView()
+    }
+}

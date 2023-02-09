c7bc75c38d6032523daa9943414b1be0ff23aede
Junping Luo <53324036+JacE070@users.noreply.github.com>
2023-02-08 21:00:11 -0800

    Finish Page 6 UI

--- Healthy Anteater/Views/AddDataView.swift
+++ Healthy Anteater/Views/AddDataView.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct AddDataView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let name = "Evan"
+        VStack(){
+            VStack(alignment:.leading){
+                Image("image_placeholder")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(30)
+                Text("Hi \(name),\nLet us know about your day")
+            }
+            .frame(maxWidth: 300)
+            
+            Spacer()
+            Button{
+                
+            } label: {
+                Text("Food Preference")
+                    .frame(maxWidth: 300, maxHeight: 50)
+            }
+            .buttonStyle(.borderedProminent)
+            Spacer()
+            Button{
+                
+            } label: {
+                Text("Update Personal Info")
+                    .frame(maxWidth: 300, maxHeight: 50)
+            }
+            .buttonStyle(.borderedProminent)
+            Spacer()
+            Button{
+                
+            } label: {
+                Text("Update Target")
+                    .frame(maxWidth: 300, maxHeight: 50)
+            }
+            .buttonStyle(.borderedProminent)
+            Spacer()
+        }
     }
 }
 

fdcf3bd51b177fbb1f7929c52c51f5fd519fc406
Junping Luo <53324036+JacE070@users.noreply.github.com>
2023-02-20 14:32:36 -0800

    Create food class

--- Healthy Anteater/Models/RecommendationItem.swift
+++ Healthy Anteater/Models/RecommendationItem.swift
@@ -9,5 +9,14 @@ import Foundation
 
 struct RecommendationItem: Identifiable {
     let id = UUID()
+    let food: Food
+}
+
+struct Food {
+    let id: Int
     let name: String
+    let description: String
+    let calories: Int
+    let contains: [String]
+    var checked: Bool
 }

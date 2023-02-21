f4a2d216744f8d01acfc8ab82b93577d5c7ac5f4
Junping Luo <53324036+JacE070@users.noreply.github.com>
2023-02-20 21:08:37 -0800

    Create UserInfo struct

--- Healthy Anteater/Models/Food.swift
+++ Healthy Anteater/Models/Food.swift
@@ -15,3 +15,13 @@ struct Food: Identifiable {
     let contains: [String]
     var checked: Bool
 }
+
+struct UserInfo {
+    let id: Int
+    let user_name: String
+    let current_weight: Float
+    let target_weight: Float
+    let rec_calories: Int
+    let taken_calories: Int
+    let food_list: [Food]
+}
